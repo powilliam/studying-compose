014389f19c593af2fb718885870f2e304756215d
powilliam <powilliam19@gmail.com>
2024-04-25 11:57:03 -0300

    messing around with theme

--- app/src/main/java/com/powilliam/studyingcompose/theming/Colors.kt
+++ app/src/main/java/com/powilliam/studyingcompose/theming/Colors.kt
@@ -1,7 +1,78 @@
 package com.powilliam.studyingcompose.theming
 
-import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
+import androidx.compose.ui.graphics.Color
 
-val applicationLightColors = lightColorScheme()
-val applicationDarkColors = darkColorScheme()
+val primaryLight = Color(0xFF23323C)
+val onPrimaryLight = Color(0xFFFFFFFF)
+val primaryContainerLight = Color(0xFF45555F)
+val onPrimaryContainerLight = Color(0xFFEBF6FF)
+val secondaryLight = Color(0xFF595F64)
+val onSecondaryLight = Color(0xFFFFFFFF)
+val secondaryContainerLight = Color(0xFFDFE4E9)
+val onSecondaryContainerLight = Color(0xFF44494E)
+val tertiaryLight = Color(0xFF372C3D)
+val onTertiaryLight = Color(0xFFFFFFFF)
+val tertiaryContainerLight = Color(0xFF5A4E60)
+val onTertiaryContainerLight = Color(0xFFFDF0FF)
+val errorLight = Color(0xFFBA1A1A)
+val onErrorLight = Color(0xFFFFFFFF)
+val errorContainerLight = Color(0xFFFFDAD6)
+val onErrorContainerLight = Color(0xFF410002)
+val backgroundLight = Color(0xFFEFEFEF)
+val onBackgroundLight = Color(0xFF1B1C1C)
+val surfaceLight = Color(0xFFEFEFEF)
+val onSurfaceLight = Color(0xFF1B1C1C)
+val surfaceVariantLight = Color(0xFFDFE3E7)
+val onSurfaceVariantLight = Color(0xFF43474B)
+val outlineLight = Color(0xFF73777B)
+val outlineVariantLight = Color(0xFFC3C7CB)
+val scrimLight = Color(0xFF000000)
+val inverseSurfaceLight = Color(0xFF303031)
+val inverseOnSurfaceLight = Color(0xFFF2F0F1)
+val inversePrimaryLight = Color(0xFFB9C9D5)
+val surfaceDimLight = Color(0xFFDBD9DA)
+val surfaceBrightLight = Color(0xFFFBF9F9)
+val surfaceContainerLowestLight = Color(0xFFFFFFFF)
+val surfaceContainerLowLight = Color(0xFFF5F3F3)
+val surfaceContainerLight = Color(0xFFEFEDEE)
+val surfaceContainerHighLight = Color(0xFFEAE8E8)
+val surfaceContainerHighestLight = Color(0xFFE4E2E2)
+
+val lightScheme = lightColorScheme(
+    primary = primaryLight,
+    onPrimary = onPrimaryLight,
+    primaryContainer = primaryContainerLight,
+    onPrimaryContainer = onPrimaryContainerLight,
+    secondary = secondaryLight,
+    onSecondary = onSecondaryLight,
+    secondaryContainer = secondaryContainerLight,
+    onSecondaryContainer = onSecondaryContainerLight,
+    tertiary = tertiaryLight,
+    onTertiary = onTertiaryLight,
+    tertiaryContainer = tertiaryContainerLight,
+    onTertiaryContainer = onTertiaryContainerLight,
+    error = errorLight,
+    onError = onErrorLight,
+    errorContainer = errorContainerLight,
+    onErrorContainer = onErrorContainerLight,
+    background = backgroundLight,
+    onBackground = onBackgroundLight,
+    surface = surfaceLight,
+    onSurface = onSurfaceLight,
+    surfaceVariant = surfaceVariantLight,
+    onSurfaceVariant = onSurfaceVariantLight,
+    outline = outlineLight,
+    outlineVariant = outlineVariantLight,
+    scrim = scrimLight,
+    inverseSurface = inverseSurfaceLight,
+    inverseOnSurface = inverseOnSurfaceLight,
+    inversePrimary = inversePrimaryLight,
+    surfaceDim = surfaceDimLight,
+    surfaceBright = surfaceBrightLight,
+    surfaceContainerLowest = surfaceContainerLowestLight,
+    surfaceContainerLow = surfaceContainerLowLight,
+    surfaceContainer = surfaceContainerLight,
+    surfaceContainerHigh = surfaceContainerHighLight,
+    surfaceContainerHighest = surfaceContainerHighestLight,
+)
--- app/src/main/java/com/powilliam/studyingcompose/theming/Theme.kt
+++ app/src/main/java/com/powilliam/studyingcompose/theming/Theme.kt
@@ -1,31 +1,11 @@
 package com.powilliam.studyingcompose.theming
 
-import android.annotation.SuppressLint
-import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
 
-private val isDynamicColorSchemeSupported = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
-
-@SuppressLint("NewApi")
 @Composable
-fun ApplicationTheme(
-    isDarkModeEnabled: Boolean = isSystemInDarkTheme(),
-    content: @Composable () -> Unit
-) {
-    val ctx = LocalContext.current
-    val colorScheme = when {
-        !isDarkModeEnabled and isDynamicColorSchemeSupported -> dynamicLightColorScheme(ctx)
-        isDarkModeEnabled and isDynamicColorSchemeSupported -> dynamicDarkColorScheme(ctx)
-        isDarkModeEnabled and !isDynamicColorSchemeSupported -> applicationDarkColors
-        else -> applicationLightColors
-    }
-
-    MaterialTheme(colorScheme) {
+fun ApplicationTheme(content: @Composable () -> Unit) {
+    MaterialTheme(colorScheme = lightScheme) {
         content()
     }
 }
